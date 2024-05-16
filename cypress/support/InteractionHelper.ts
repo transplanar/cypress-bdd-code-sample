563082a8d3aa264646cfa5e52984779acfd62cbc
Glen <glen@glencooney.com>
2024-05-16 12:46:38 -0700

    Start of math operation tests

---
+++ cypress/support/InteractionHelper.ts
@@ -0,0 +1,6 @@
+/**
+ * Takes a number sentence and uses that to simulate click interactions with the page
+ *
+ * @param sentence
+ */
+export function ParseNumberSentenceToClicks(sentence: string): void {}
---
+++ cypress/support/PageData/CalculatorUI.ts
@@ -0,0 +1,7 @@
+enum UIElement {
+  Display = 'display',
+}
+
+export calculatorUISelectors = new Map<UIElement, string>([
+  [UIElement.Display, '#cwos'],
+]);
